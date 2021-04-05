23feefb21123dfa81fb648da92028e13b7e189a0
AliJonMadaminov <you@example.com>
2021-04-05 17:36:33 +0500

    first commit

---
+++ app/src/main/java/com/example/aliplayer/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aliplayer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aliplayer.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
